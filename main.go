30812dd996b657f015d0c53fa612611d2b8932f9
Mark Joven Reyes <jovenreyes020803@gmail.com>
2021-11-15 17:11:36 +0800

    Create a Function

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
+func hello() {
+	fmt.Println("Hello!")
+}
+
 func main() {
 	fmt.Println("Hello, People!")
 
 	for i := 0; i < 15; i++ {
 		fmt.Println(i)
 	}
+
+	hello()
 }
